75988a3d69d3c6a1351566736373bc49e5844482
Datto6 <61701645+Datto6@users.noreply.github.com>
2026-04-29 18:07:16 -0300

    tudo no repositorio do nilo, idem

--- Insercao_Arquivos.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-
-arquivos=st.file_uploader("Insira aqui a pasta com os arquivos que voce quer analisar", type="csv", accept_multiple_files="directory")
-for arq in arquivos:
-    df=pd.read_csv(arq,sep=';')
-    st.session_state.dataframes.append(df)
