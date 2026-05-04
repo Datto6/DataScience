aea7bd4d680e78526a28b7ad66f89f5aded11452
unknown <rafaosbcarvalho@gmail.com>
2026-05-04 13:33:40 -0300

    Preenche usando kNN apenas o Saving Accounts, e usa -1 como valor faltante em Checking Accounts. Acuracia de 67%

---
+++ trabalho/modelkNN.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+from sklearn import tree  # Arvore de decisão e plot tree
+from sklearn.metrics import accuracy_score   # Acurácia
+from sklearn.preprocessing import OrdinalEncoder,OneHotEncoder  # Transformar coluna ordinária
+from sklearn.model_selection import train_test_split  # Separar a parte de teste
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay  # Matriz de confusão
+import matplotlib.pyplot as plt  # Plot na tabela
+from sklearn.impute import KNNImputer
+
+df=pd.read_csv("class_german_credit.csv")
+
+# Sex
+df['Sex'] = (df['Sex'] == 'male').astype(int) # female -> 0; male -> 1;
+
+# Housing
+encoder = OrdinalEncoder(categories=[['free', 'rent', 'own']])
+df['Housing'] = encoder.fit_transform(df[['Housing']])
+
+# Saving Accounts
+encoder = OrdinalEncoder(handle_unknown='use_encoded_value',unknown_value= -1,categories=[['little', 'moderate', 'quite rich', 'rich']])
+imputer = KNNImputer(n_neighbors=5)
+
+df['Saving accounts']=encoder.fit_transform(df[['Saving accounts']]) #converts to numbers
+
+df['Saving accounts']=imputer.fit_transform(df[['Saving accounts']]) #takes care of missing values
+
+
+
+# Checking Account
+encoder = OrdinalEncoder(handle_unknown='use_encoded_value',unknown_value= -1,categories=[['little', 'moderate', 'rich']])
+df['Checking account'] = encoder.fit_transform(df[['Checking account']])
+
+# Purpose
+purpose_encoder = OneHotEncoder(handle_unknown='ignore')
+
+purpose_encoder.fit(df[['Purpose']]) #fit
+
+encoded = purpose_encoder.transform(df[['Purpose']]).toarray() # transform
+
+# turn into DataFrame with proper column names
+encoded_df = pd.DataFrame(encoded, columns=purpose_encoder.get_feature_names_out(['Purpose']))
+
+df = df.drop(columns=['Purpose']) # drop original column
+
+# concatenate
+df = pd.concat([df, encoded_df], axis=1)
+
+# Risk
+df['Risk'] = (df['Risk'] == 'good').astype(int) # bad -> 0; good -> 1;
+
+
+X = df.drop('Risk', axis=1)
+y = df['Risk']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify = y)
+
+clf = tree.DecisionTreeClassifier(random_state=42)
+
+# Treinamento
+clf.fit(X_train, y_train)
+
+# Teste
+y_pred = clf.predict(X_test)
+
+acuracia = accuracy_score(y_test, y_pred)
+print(f'A acurácia do modelo foi de {acuracia*100:.2f}%')
